3da4662e740afdc74adf70d1a1e5ad818f364730
sagar <5agarm5hetty05@gmail.com>
2023-09-14 23:16:28 +0530

    added image upload api route

---
+++ src/app/api/upload/route.js
@@ -0,0 +1,35 @@
+import mime from "mime";
+import { join } from "path";
+import { stat, mkdir, writeFile } from "fs/promises";
+import { NextResponse } from 'next/server'
+
+export async function POST(request){
+    const formData = await request.formData();
+    const file = formData.get("file");
+
+    const buffer = Buffer.from(await file.arrayBuffer());
+    const relativeUploadDir = `/uploads`;
+    const uploadDir = join(process.cwd(), "public", relativeUploadDir);
+
+    try {
+        const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+        const filename = `${file.name.replace(
+          /\.[^/.]+$/,
+          ""
+        )}-${uniqueSuffix}.${mime.getExtension(file.type)}`;
+        await writeFile(`${uploadDir}/${filename}`, buffer);
+        return NextResponse.json({ fileUrl: `${relativeUploadDir}/${filename}` });
+      } catch (e) {
+        console.error("Error while trying to upload a file\n", e);
+        return NextResponse.json(
+          { error: "Something went wrong." },
+          { status: 500 }
+        );
+      }
+}
+
+export const config = {
+    api: {bodyParser: false},
+
+}
+
